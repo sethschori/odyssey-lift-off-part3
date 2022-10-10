78724f8efe167f886cab2af4eb07adc6ba89e09b
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-10 15:16:25 -0400

    Add GET_TRACK gql query to Track component

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -3,6 +3,30 @@ import React from "react";
 import {gql, useQuery} from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 
+const GET_TRACK = gql`
+    query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+        id
+        title
+        author {
+        id
+        name
+        photo
+        }
+        thumbnail
+        length
+        modulesCount
+        description
+        numberOfViews
+        modules {
+        id
+        title
+        length
+        }
+    }
+    }
+`
+
 const Track = ({ trackId }) => {
     return <Layout></Layout>
 }
