50dd053bc236d31c859ab5d4b30ead41e3d58720
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-10 15:14:29 -0400

    Add initial Track component

---
+++ client/src/pages/track.js
@@ -0,0 +1,10 @@
+import { ApolloClient } from "@apollo/client";
+import React from "react";
+import {gql, useQuery} from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+
+const Track = ({ trackId }) => {
+    return <Layout></Layout>
+}
+
+export default Track;
